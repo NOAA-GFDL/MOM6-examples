53669435f6354e96da9ec5e699249ae566514b65
John Krasting <John.Krasting@noaa.gov>
2016-09-06 09:25:16 -0400

    Added clim options for TS_drift.py

--- tools/analysis/TS_drift.py
+++ tools/analysis/TS_drift.py
@@ -21,6 +21,8 @@ def run():
   main(cmdLineArgs)
 
 def main(cmdLineArgs,stream=None):
+  print cmdLineArgs.climT
+  
   rootGroupT = netCDF4.MFDataset( cmdLineArgs.annual_directory + '/*.thetao_xyave.nc' )
   rootGroupS = netCDF4.MFDataset( cmdLineArgs.annual_directory + '/*.so_xyave.nc' )
   if 'thetao_xyave' not in rootGroupT.variables: raise Exception('Could not find "thetao_xyave" files "%s"'%(cmdLineArgs.annual_directory))
@@ -57,14 +59,14 @@ def main(cmdLineArgs,stream=None):
   m6plot.ztplot( T, timeT, zt, splitscale=[0., -1000., -6500.],
       suptitle=suptitle, title='Potential Temperature [C]',
       extend='both', colormap='dunnePM', autocenter=True,
-      save=objOut)
+      clim=cmdLineArgs.climT,save=objOut)
 
   if stream != None: objOut = stream[1]
   else: objOut = cmdLineArgs.outdir+'/S_drift.png'
   m6plot.ztplot( S, timeS, zt, splitscale=[0., -1000., -6500.],
       suptitle=suptitle, title='Salinity [psu]',
       extend='both', colormap='dunnePM', autocenter=True,
-      save=objOut)
+      clim=cmdLineArgs.climS,save=objOut)
 
 if __name__ == '__main__':
   run()
