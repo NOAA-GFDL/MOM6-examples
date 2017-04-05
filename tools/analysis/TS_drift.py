32c07763740dcfcbeeb76b2f88fd0782460023be
John Krasting <John.Krasting@noaa.gov>
2017-04-05 15:34:10 -0400

    Changed 'zt' to 'z_l' in TS_drift plot script
      - Also added dynamic image buffers

--- tools/analysis/TS_drift.py
+++ tools/analysis/TS_drift.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import io
 import netCDF4
 import numpy
 import m6plot
@@ -12,7 +13,7 @@ except: raise Exception('This version of python is not new enough. python 2.7 or
 
 def run():
   parser = argparse.ArgumentParser(description='''Script for plotting depth vs. time plots of temperature and salinity drift''')
-  parser.add_argument('annual_directory', type=str, help='''Directory containing annual time series thetao and so xyave files''')
+  parser.add_argument('infile', type=str, help='''Directory containing annual time series thetao and so xyave files''')
   parser.add_argument('-l','--label', type=str, default='', help='''Label to add to the plot.''')
   parser.add_argument('-s','--suptitle', type=str, default='', help='''Super-title for experiment.  Default is to read from netCDF file.''')
   parser.add_argument('-o','--outdir', type=str, default='.', help='''Directory in which to place plots.''')
@@ -20,51 +21,56 @@ def run():
   cmdLineArgs = parser.parse_args()
   main(cmdLineArgs)
 
-def main(cmdLineArgs,stream=None):
-  rootGroupT = netCDF4.MFDataset( cmdLineArgs.annual_directory + '/*.thetao_xyave.nc' )
-  rootGroupS = netCDF4.MFDataset( cmdLineArgs.annual_directory + '/*.so_xyave.nc' )
-  if 'thetao_xyave' not in rootGroupT.variables: raise Exception('Could not find "thetao_xyave" files "%s"'%(cmdLineArgs.annual_directory))
-  if 'so_xyave' not in rootGroupS.variables: raise Exception('Could not find "so_xyave" files "%s"'%(cmdLineArgs.annual_directory))
+def main(cmdLineArgs,stream=False):
+  if not isinstance(cmdLineArgs.infile,list):
+    cmdLineArgs.infile = [cmdLineArgs.infile]
+  rootGroupT = [x+'.thetao_xyave.nc' for x in cmdLineArgs.infile]  
+  rootGroupS = [x+'.so_xyave.nc' for x in cmdLineArgs.infile]  
+  rootGroupT = netCDF4.MFDataset( rootGroupT )
+  rootGroupS = netCDF4.MFDataset( rootGroupS )
+  if 'thetao_xyave' not in rootGroupT.variables: raise Exception('Could not find "thetao_xyave" files "%s"'%(cmdLineArgs.infile))
+  if 'so_xyave' not in rootGroupS.variables: raise Exception('Could not find "so_xyave" files "%s"'%(cmdLineArgs.infile))
 
-  zt = rootGroupT.variables['zt'][::-1] * -1
+  if 'zt' in rootGroupT.variables.keys():
+    zt = rootGroupT.variables['zt'][::-1] * -1
+  elif 'z_l' in rootGroupT.variables.keys():
+    zt = rootGroupT.variables['z_l'][::-1] * -1
   timeT = rootGroupT.variables['time']
   timeS = rootGroupS.variables['time']
   timeT = numpy.array([int(x.year) for x in netCDF4.num2date(timeT[:],timeT.units,calendar=timeT.calendar)])
   timeS = numpy.array([int(x.year) for x in netCDF4.num2date(timeS[:],timeS.units,calendar=timeS.calendar)])
 
-  if cmdLineArgs.trange != None:
-    start = list(timeT).index(cmdLineArgs.trange[0])
-    end = list(timeT).index(cmdLineArgs.trange[1])
-  else:
-    start = 0
-    end = -1
-
   variable = rootGroupT.variables['thetao_xyave']
-  T = variable[start:end] - variable[start]
-  T = T[:,::-1]
-  timeT = timeT[start:end]
+  T = variable[:]
+  T = T-T[0]
 
   variable = rootGroupS.variables['so_xyave']
-  S = variable[start:end] - variable[start]
-  S = S[:,::-1]
-  timeS = timeS[start:end]
+  S = variable[:]
+  S = S-S[0]
 
   if cmdLineArgs.suptitle != '':  suptitle = cmdLineArgs.suptitle + ' ' + cmdLineArgs.label
   else: suptitle = rootGroupT.title + ' ' + cmdLineArgs.label
 
-  if stream != None: objOut = stream[0]
+  imgbufs = []
+
+  if stream is True: objOut = io.BytesIO()
   else: objOut = cmdLineArgs.outdir+'/T_drift.png'
-  m6plot.ztplot( T, timeT, zt, splitscale=[0., -1000., -6500.],
+  m6plot.ztplot( T, timeT, zt, splitscale=[0., -2000., -6500.],
       suptitle=suptitle, title='Potential Temperature [C]',
       extend='both', colormap='dunnePM', autocenter=True,
-      clim=cmdLineArgs.climT,save=objOut)
+      save=objOut)
+  if stream is True: imgbufs.append(objOut)
 
-  if stream != None: objOut = stream[1]
+  if stream is True: objOut = io.BytesIO()
   else: objOut = cmdLineArgs.outdir+'/S_drift.png'
-  m6plot.ztplot( S, timeS, zt, splitscale=[0., -1000., -6500.],
+  m6plot.ztplot( S, timeS, zt, splitscale=[0., -2000., -6500.],
       suptitle=suptitle, title='Salinity [psu]',
       extend='both', colormap='dunnePM', autocenter=True,
-      clim=cmdLineArgs.climS,save=objOut)
+      save=objOut)
+  if stream is True: imgbufs.append(objOut)
+
+  if stream is True:
+    return imgbufs
 
 if __name__ == '__main__':
   run()
